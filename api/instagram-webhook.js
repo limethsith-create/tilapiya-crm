1d66e3d3d0f5bd1cfbf06323a0b5fdb3aec46935
limethsith-create <limethsith@gmail.com>
2026-06-14 20:44:35 +0530

    Add files via upload

--- api/instagram-webhook.js
+++ api/instagram-webhook.js
@@ -25,7 +25,7 @@ const {
 
 const VERIFY_TOKEN = process.env.IG_VERIFY_TOKEN || process.env.FB_VERIFY_TOKEN || process.env.WEBHOOK_VERIFY_TOKEN || 'tilapiya_meta_2026';
 const IG_APP_SECRET = process.env.IG_APP_SECRET || process.env.FB_APP_SECRET || process.env.META_APP_SECRET || 'cfa507befc2038d69f0c8e72a66e2bae';
-const IG_PAGE_TOKEN = process.env.IG_PAGE_TOKEN || 'IGAAWEtLoAjLlBZAFoteks1WXJIdU1NRlBRTUlvdDlyR05sTjBNUzJfYWQ4SUpOMVY0dGMxd3JZAWl9ON3Q5MHZAhUjlXb3hoRFp3ZAWNJbjQtSEdTLW5mXzJvNlI5blJ2QUhTMHpOV2xLczZAZAc2tRY1hNdUt3ek9EVUREUnQ5dUJwZAwZDZD';
+const IG_PAGE_TOKEN = process.env.IG_PAGE_TOKEN || 'IGAAWEtLoAjLlBZAGFoOFcxSEFVZAnVCVUc2a3B3UmNHQVJHSHE4S1NocF9td1g3U2FrX3lsTlNycTZAjZA2owMVJXa0xnOGxPbHNsTDBTLVJ3cm9ZASy01M1dTR1N0YkxRZAV9rc0ZA5dHBxWFYzVHpoSVUxc3FSS1IwLUM0cGhMcjFJZAwZDZD';
 const IG_PAGE_ID = process.env.IG_PAGE_ID; // optional
 
 module.exports = async function handler(req, res) {
